43816411d2464fbe0e3e61461cf80b39effe963a
SergeyLashenko <sergeylashenco@gmail.com>
2020-03-08 16:48:35 +0200

    Add Allure

--- src/test/java/appmanager/ApplicationManager.java
+++ src/test/java/appmanager/ApplicationManager.java
@@ -1,6 +1,8 @@
 package appmanager;
 
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -81,6 +83,10 @@ public class ApplicationManager {
     }
     public String getProperty(String key){
         return properties.getProperty(key);
+    }
+
+    public  byte [] takeScreenshot() {
+       return  ((TakesScreenshot) wd ).getScreenshotAs(OutputType.BYTES);
     }
 
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,22 +4,23 @@ import appmanager.ApplicationManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+@Listeners(MyTestListener.class)
 public class TestBase {
+
     Logger logger = LogManager.getLogger(TestBase.class);
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
     @BeforeSuite
-    public void setUp() throws IOException {
+    public void setUp(ITestContext context) throws IOException {
         app.init();
+        context.setAttribute("app" , app);
     }
 
     @AfterSuite(alwaysRun = true)
